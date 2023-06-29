b48e70ff4a3247fe30333ef670ab8fd1a777f7fd
Josias Pereira Bueno <66849846+buenojosias@users.noreply.github.com>
2023-06-28 22:42:02 -0300

    Criada UI da interface

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,11 +1,13 @@
-@props(['active'])
+@props(['active','icon'])
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? 'my-1.5 text-base text-gray-900 font-normal rounded bg-slate-200 hover:bg-gray-300 group transition duration-75 flex items-center p-2'
+            : 'my-1.5 text-base text-gray-900 font-normal rounded hover:bg-gray-100 group transition duration-75 flex items-center p-2';
+            // : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
-    {{ $slot }}
+    <i class="fa fa-{{$icon}} text-sm w-5 h-5 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"></i>
+    <span class="ml-2">{{ $slot }}</span>
 </a>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,38 +1,39 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name', 'CateSis') }}</title>
+    <link rel="stylesheet" href="{{ asset('icons/all.min.css') }}">
+    <style>[x-cloak] { display: none !important; }</style>
+    @wireUiScripts
+    {{-- <link rel="stylesheet" href="{{ asset('build/assets/app-e901d665.css') }}"> --}}
+    {{-- <script src="{{ asset('build/assets/?.js') }}" defer></script> --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+    @livewireScripts
+</head>
+<body x-data="{ showsidebar: false, usermenu: false }" class="bg-gray-200 antialiased">
+    @include('layouts.navstack')
+    <div class="flex overflow-hidden pt-12">
+        @include('layouts.navigation')
+        <div class="h-full w-full relative overflow-y-auto lg:ml-64">
             @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <header class="bg-white shadow px-4">
+                    <div class="max-w-5xl mx-auto py-4">
                         {{ $header }}
                     </div>
                 </header>
             @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
+            <main class="py-4 sm:py-6 lg:py-8 px-4">
+                <div class="max-w-5xl mx-auto">
+                    {{ $slot }}
+                </div>
             </main>
         </div>
-        @livewireScripts
-    </body>
+    </div>
+    @stack('scripts')
+</body>
 </html>
